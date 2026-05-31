2c4aa5ff88d83fcc9efd94259b754cc19b5f61f8
K.Sailaja <sailajak473@gmail.com>
2026-05-31 16:35:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 import joblib
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 CORS(app)
 
 BASE_DIR     = os.path.dirname(os.path.abspath(__file__))
@@ -84,4 +84,7 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
